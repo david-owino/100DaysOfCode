02507c852d9985c3c7f6e0033482a1e660e88326
David Owino <daveowino753@gmail.com>
2025-05-22 21:12:15 +0300

    add script to determine if number is even or odd

---
+++ Day 2/oddeven.py
@@ -0,0 +1,6 @@
+#script to check if a number is odd or even
+number = int(input("Enter a number"))
+if number % 2 == 0:
+    print(f"The number {number} is even")
+else:
+    print(f"The number {number} is odd")
