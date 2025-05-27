c11920ca555ad05ff0e960e67be51091b8561756
David Owino <daveowino753@gmail.com>
2025-05-27 21:21:18 +0300

    add hangman_words

---
+++ Day 5/hangman_words.py
@@ -0,0 +1,14 @@
+# hangman_words.py
+
+word_list = [
+    "python",
+    "developer",
+    "hangman",
+    "function",
+    "variable",
+    "loop",
+    "keyboard",
+    "exception",
+    "flutter",
+    "coffee"
+]
